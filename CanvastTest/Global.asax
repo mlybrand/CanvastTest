d2b799853bb4d85225a4be66c96f61152812269a
Mark Lybrand <mlybrand@gmail.com>
2015-04-14 22:50:45 -0700

    initial commit

---
+++ CanvastTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CanvastTest.MvcApplication" Language="C#" %>
---
+++ CanvastTest/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CanvastTest.Startup))]
+namespace CanvastTest
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
