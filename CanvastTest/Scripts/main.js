11dcf3080bdd025079231136aa67d42e7d748c7f
Mark Lybrand <mlybrand@gmail.com>
2015-04-23 20:03:47 -0700

    bringing project up to date with git repo

---
+++ CanvastTest/Scripts/main.js
@@ -0,0 +1,34 @@
+﻿$(document).ready(function () {
+    if (!Modernizr.canvas) {
+        alert("This browser does not support the canvas tag.");
+        return;
+    }
+    var $canvas = $("#main");
+    var context = $canvas[0].getContext("2d");
+    var w;
+    var h;
+    var maxVelocity = 10;
+    var points = [];
+    var radians = 0;
+    var segments = 3;
+    var pointsCount = 15;
+
+    // Image preparation placeholder
+    
+    function resize() {
+        w = $canvas.width();
+        h = $canvas.height();
+        $canvas.attr("width", w);
+        $canvas.attr("height", h);
+        // Resize function body placeholder
+    }
+    $(window).resize(resize);
+    resize();
+
+    // Randomizer placeholder
+
+    function frame() {
+        // Frame function body placeholder
+    }
+    frame();
+});
